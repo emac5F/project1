cd1394067fb8f0ca993d6ac6d18580e39bd1390f
emac5F <emac109508@gmail.com>
2025-04-30 18:14:09 +0200

    se agrego nuevo componente header y se mejoro la funcionalidad del usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,29 @@ import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { CommentsComponent } from "./comments/comments.component"; // Import the UserComponent
 
+import { HeaderComponent } from './components/header/header.component'; //trabajando con la carpeta de components
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, CommentsComponent],
+  imports: [RouterOutlet, UserComponent, CommentsComponent, HeaderComponent, FormsModule], // Import the UserComponent here
   templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+
   // template: `<h1>Hola mudo desde {{city.toUpperCase()}} {{2+1}}</h1>
   //   <app-user></app-user><app-user/>`,
-
-  styleUrl: './app.component.css'
   // styles: ``,
 })
 export class AppComponent {
   title = 'project1';
   city = 'Barcelona';
+
+  // components
+  userName: string = 'Edith'; // Example user name
+  
+  number: number = 0; // Example number
+  addOne() {
+    this.number++; // Increment the number by 1
+  }
 }
