e30f1f766607cbe4d239c6997723710f77d0a3b8
emac5F <emac109508@gmail.com>
2025-05-08 00:32:46 -0400

    agregar componentes Home, MovieList y MovieForm con sus respectivas plantillas y pruebas unitarias; configurar rutas para navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieFormPageComponent } from './pages/movie-form-page/movie-form-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // trabajando con RUTAS - carpeta pages
+    {path: '', component: HomeComponent},
+    {path: 'movies', component: MovieListComponent},
+    {path: 'create', component: MovieFormPageComponent}
+];
