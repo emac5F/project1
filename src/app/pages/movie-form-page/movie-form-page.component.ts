dd3534a334fb0cc12fa9031056552196aa128eec
emac5F <emac109508@gmail.com>
2025-05-08 01:26:08 -0400

    agregar componente Header con su lógica, plantilla y estilos; actualizar páginas para incluir el encabezado

--- src/app/pages/movie-form-page/movie-form-page.component.ts
+++ src/app/pages/movie-form-page/movie-form-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FormsComponent } from '../../components/forms/forms.component';
 
 @Component({
   selector: 'app-movie-form-page',
-  imports: [],
+  imports: [HeaderComponent, FormsComponent],
   templateUrl: './movie-form-page.component.html',
   styleUrl: './movie-form-page.component.css'
 })
